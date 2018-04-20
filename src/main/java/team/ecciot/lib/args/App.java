7663cd985627a298584f6b1260002b0cea3aeda9
Guijie Lee <landriesnidis@yeah.net>
2018-04-20 19:33:24 +0800

    测试类

--- src/main/java/team/ecciot/lib/args/App.java
+++ src/main/java/team/ecciot/lib/args/App.java
@@ -1,13 +1,40 @@
 package team.ecciot.lib.args;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!!" );
-    }
+import java.util.List;
+
+import team.ecciot.lib.args.annotation.CmdArgsAnnotation;
+import team.ecciot.lib.args.annotation.EventArgsAnnotation;
+import team.ecciot.lib.args.utils.ClassUtil;
+
+public class App {
+	public static void main(String[] args) {
+		
+		System.out.println("package : team.ecciot.lib.args.event");
+		System.out.println();
+		
+		List<Class<?>> lst = ClassUtil.getClasses("team.ecciot.lib.args");
+		
+		for(Class<?> c:lst){
+			try{
+				if(c.getAnnotations().length>0){
+					if(c.isAnnotationPresent(CmdArgsAnnotation.class)){
+						CmdArgsAnnotation an = c.getAnnotation(CmdArgsAnnotation.class);
+						System.out.println(String.format("%s\t%s\t%s", c.getName(),CmdArgsAnnotation.class.getName(),an.action()));
+					}else if(c.isAnnotationPresent(EventArgsAnnotation.class)){
+						EventArgsAnnotation an = c.getAnnotation(EventArgsAnnotation.class);
+						System.out.println(String.format("%s\t%s\t%s", c.getName(),EventArgsAnnotation.class.getName(),an.action()));
+					}else{
+						System.out.println(c.getName() + "\t" + "other");
+					}
+				}
+			}catch (ClassCastException e) {
+				System.out.println(c.getName());
+			}
+			
+		}
+		
+		
+	}
+	
+	
 }
