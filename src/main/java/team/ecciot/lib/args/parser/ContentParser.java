8f130cf838aaa9711848735265ba6b71149ee9df
Guijie Lee <landriesnidis@yeah.net>
2018-05-03 20:51:57 +0800

    传入回调接口后不需要再指定回调接口的类

--- src/main/java/team/ecciot/lib/args/parser/ContentParser.java
+++ src/main/java/team/ecciot/lib/args/parser/ContentParser.java
@@ -2,58 +2,13 @@ package team.ecciot.lib.args.parser;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.HashMap;
-import java.util.List;
 
 import com.alibaba.fastjson.JSONObject;
 
-import team.ecciot.lib.args.annotation.ArgsAnnotation;
 import team.ecciot.lib.args.callback.IBaseParserCallback;
-import team.ecciot.lib.args.callback.ICmsEventCallback;
-import team.ecciot.lib.args.callback.IDeviceEventCallback;
-import team.ecciot.lib.args.callback.IRtcEventCallback;
-import team.ecciot.lib.args.callback.IServerEventCallback;
-import team.ecciot.lib.args.callback.ITerminalEventCallback;
 import team.ecciot.lib.args.exception.ParserException;
 import team.ecciot.lib.args.model.BaseEccArgs;
-import team.ecciot.lib.args.utils.ClassUtil;
-
-/**
- * 扫描参数模型的工具类
- * @author Landriesnidis
- *
- */
-class ParserUtils{
-	public static HashMap<String, String> HM_ACTION_ARGS;
-	
-	static{
-		try {
-			HM_ACTION_ARGS =scanPackage("team.ecciot.lib.args.model") ;
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	private static HashMap<String, String> scanPackage(String pkg) throws Exception {
-		List<Class<?>> lst = ClassUtil.getClasses(pkg);
-		HashMap<String, String> hm = new HashMap<String, String>();
-		
-		for(Class<?> c:lst){
-			if(c.getAnnotations().length>0){
-				if(c.isAnnotationPresent(ArgsAnnotation.class)){
-					ArgsAnnotation an = c.getAnnotation(ArgsAnnotation.class);
-					String action = an.action();
-					if(hm.containsKey(action)){
-						throw new Exception(String.format("action:%s has already been registered.", action));
-					}else{
-						hm.put(action, c.getName());
-					}
-				}
-			}
-		}
-		return hm;
-	}
-}
+import team.ecciot.lib.args.utils.ParserUtils;
 
 public class ContentParser {
 
@@ -64,30 +19,10 @@ public class ContentParser {
 		this.callback = callback;
 		this.clazz = callbackClass;
 	}
-
-	public ContentParser(ICmsEventCallback callback) {
-		this.callback = callback;
-		this.clazz = ICmsEventCallback.class;
-	}
 	
-	public ContentParser(IDeviceEventCallback callback) {
+	public ContentParser(IBaseParserCallback callback){
 		this.callback = callback;
-		this.clazz = IDeviceEventCallback.class;
-	}
-	
-	public ContentParser(IRtcEventCallback callback) {
-		this.callback = callback;
-		this.clazz = IRtcEventCallback.class;
-	}
-	
-	public ContentParser(IServerEventCallback callback) {
-		this.callback = callback;
-		this.clazz = IServerEventCallback.class;
-	}
-	
-	public ContentParser(ITerminalEventCallback callback) {
-		this.callback = callback;
-		this.clazz = ITerminalEventCallback.class;
+		this.clazz = callback.getClass();
 	}
 	
 	public boolean parse(String action, String content) throws ParserException {
