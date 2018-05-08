eaaea7c54c0971ad8a62f8583c649ebb9ca14829
Guijie Lee <landriesnidis@yeah.net>
2018-05-09 04:20:21 +0800

    更新用于测试的回调事件处理器

--- src/main/java/team/ecciot/lib/args/TestCallbcakHandler.java
+++ src/main/java/team/ecciot/lib/args/TestCallbcakHandler.java
@@ -1,27 +1,17 @@
 package team.ecciot.lib.args;
 
+import team.ecciot.lib.args.callback.IBaseParserCallback;
 import team.ecciot.lib.args.callback.ICheckIdentityCallback;
-import team.ecciot.lib.args.callback.IServerEventCallback;
 import team.ecciot.lib.args.model.impl.APIKeyInvalidArgs;
 import team.ecciot.lib.args.model.impl.APIKeyVerifiedArgs;
-import team.ecciot.lib.args.model.impl.AlarmArgs;
 import team.ecciot.lib.args.model.impl.AskIdentityArgs;
-import team.ecciot.lib.args.model.impl.BindDeviceArgs;
-import team.ecciot.lib.args.model.impl.ControlItemArgs;
-import team.ecciot.lib.args.model.impl.DeviceStateChangedArgs;
 
-public class TestCallbcakHandler implements IServerEventCallback,ICheckIdentityCallback{
+public class TestCallbcakHandler implements ICheckIdentityCallback,IBaseParserCallback{
 
 	@Override
 	public void InvalidActionInstruction(String action, String content) {
 		System.out.println(String.format("未知的Action：%s，内容为：%s。", action, content));
 	}
-
-	@Override
-	public void Device_Alarm(AlarmArgs args) {
-		System.out.println(args);
-	}
-
 
 	@Override
 	public void RTC_AskIdentity(AskIdentityArgs args) {
@@ -36,21 +26,6 @@ public class TestCallbcakHandler implements IServerEventCallback,ICheckIdentityC
 	@Override
 	public void RTC_APIKeyInvalid(APIKeyInvalidArgs args) {
 		System.out.println(args);
-	}
-
-	@Override
-	public void Terminal_ControlItem(ControlItemArgs args) {
-		System.out.println(args);
-	}
-
-	@Override
-	public void RTC_DeviceStateChanged(DeviceStateChangedArgs args) {
-		System.out.println(args);		
-	}
-
-	@Override
-	public void Terminal_BindDevice(BindDeviceArgs args) {
-		System.out.println(args);		
 	}
 
 }
