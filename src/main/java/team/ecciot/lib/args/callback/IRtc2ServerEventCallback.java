bd7b669c761e31ef4e4aab7ed334e14182338280
Guijie Lee <landriesnidis@yeah.net>
2018-05-09 04:18:49 +0800

    将回调接口的方法重新划分

--- src/main/java/team/ecciot/lib/args/callback/IRtc2ServerEventCallback.java
+++ src/main/java/team/ecciot/lib/args/callback/IRtc2ServerEventCallback.java
@@ -1,7 +1,11 @@
 package team.ecciot.lib.args.callback;
 
 import team.ecciot.lib.args.model.impl.DeviceStateChangedArgs;
+import team.ecciot.lib.args.model.impl.ReturnDeviceQueryResultArgs;
+import team.ecciot.lib.args.model.impl.ReturnTerminalQueryResultArgs;
 
 public interface IRtc2ServerEventCallback extends IBaseParserCallback {
 	void RTC_DeviceStateChanged(DeviceStateChangedArgs args);
+	void RTC_ReturnDeviceQueryResult(ReturnDeviceQueryResultArgs args);
+	void RTC_ReturnTerminalQueryResult(ReturnTerminalQueryResultArgs args);
 }
--- src/main/java/team/ecciot/lib/args/callback/IServer2RtcEventCallback.java
+++ src/main/java/team/ecciot/lib/args/callback/IServer2RtcEventCallback.java
@@ -1,11 +1,11 @@
 package team.ecciot.lib.args.callback;
 
-import team.ecciot.lib.args.model.impl.ChangeDeviceStateArgs;
+import team.ecciot.lib.args.model.impl.CloseDeviceArgs;
 import team.ecciot.lib.args.model.impl.QueryOnlineDevicesArgs;
-import team.ecciot.lib.args.model.impl.QueryOnlineTerminalArgs;
+import team.ecciot.lib.args.model.impl.QueryOnlineTerminalsArgs;
 
 public interface IServer2RtcEventCallback extends IBaseParserCallback {
-	void Server_ChangeDeviceState(ChangeDeviceStateArgs args);
+	void Server_CloseDevice(CloseDeviceArgs args);
 	void Server_QueryOnlineDevices(QueryOnlineDevicesArgs args);
-	void Server_QueryOnlineTerminal(QueryOnlineTerminalArgs args);
+	void Server_QueryOnlineTerminals(QueryOnlineTerminalsArgs args);
 }
