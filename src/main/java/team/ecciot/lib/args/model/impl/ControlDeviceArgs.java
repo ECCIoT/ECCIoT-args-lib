cc4a0ac9924b7a64f91aeb4390a107d2aa5723c7
Guijie Lee <landriesnidis@yeah.net>
2018-05-11 12:06:31 +0800

    重命名参数类型

--- src/main/java/team/ecciot/lib/args/model/impl/ControlDeviceArgs.java
+++ src/main/java/team/ecciot/lib/args/model/impl/ControlDeviceArgs.java
@@ -6,15 +6,14 @@ import team.ecciot.lib.args.annotation.ArgsAnnotation;
 import team.ecciot.lib.args.exception.ParserException;
 import team.ecciot.lib.args.model.BaseEccArgs;
 
-@ArgsAnnotation(action = "Terminal_ControlItem")
-public class ControlItemArgs extends BaseEccArgs {
-	
+@ArgsAnnotation(action = "Terminal_ControlDevice")
+public class ControlDeviceArgs extends BaseEccArgs {
 	
 	private String itemID;
     private String atCmd;
     
-    public ControlItemArgs() {}
-    public ControlItemArgs(String content) throws ParserException {
+    public ControlDeviceArgs() {}
+    public ControlDeviceArgs(String content) throws ParserException {
 		super(content);
 	}
     
